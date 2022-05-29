47a55fe2add91c12fe6e290f97a2857499eb0774
codetronik <codetronik@gmail.com>
2022-05-29 16:54:19 +0900

    vs2022 build

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := injector
-LOCAL_SRC_FILES := main.cpp injector.cpp utils.cpp
-LOCAL_LDFLAGS += -pie
+LOCAL_SRC_FILES := main.cpp utils.cpp injector.cpp
 
 include $(BUILD_EXECUTABLE)
+
+all: post-build
+
+post-build:
+	adb push ../libs/$(TARGET_ARCH_ABI)/$(LOCAL_MODULE) /data/local/tmp
+	adb shell "chmod 777 /data/local/tmp/$(LOCAL_MODULE)"
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,3 @@
 APP_ABI := arm64-v8a
 APP_PLATFORM = android-30
+APP_STL := c++_static
